2f0ff4e56cb75e61385caadebde233bfa5949cd7
MD Al Mamun Galib <mdalmamungalib3929@gmail.com>
2025-01-28 22:26:13 +0600

    auth error

--- src/Contexts/AuthProvider/AuthProvider.jsx
+++ src/Contexts/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,10 @@
 import { getAuth } from "firebase/auth";
 import React, { createContext } from "react";
-import app from "../../Firebase/firebase.config";
+import { auth } from "../../Firebase/firebase.config";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const auth = getAuth(app);
-  const authInfo = {};
+  const authInfo = { auth };
   return (
     <AuthContext.Provider value={authInfo}>
       {children}
--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// Firebase configuration from environment variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
@@ -10,7 +11,10 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_appId,
 };
 
+// Initialize Firebase App
 const app = initializeApp(firebaseConfig);
 
+// Export the Firebase Auth instance
+export const auth = getAuth(app);
 
 export default app;
