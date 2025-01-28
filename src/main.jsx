ade22cbcc4341ed16c1dd2b5c58ca860b07b5185
MD Al Mamun Galib <mdalmamungalib3929@gmail.com>
2025-01-28 22:13:53 +0600

    firebase integrate

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes.jsx";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.js";
+import AuthProvider from "./Contexts/AuthProvider/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={routes} />
-    </Provider>
+    <AuthProvider>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
+    </AuthProvider>
   </React.StrictMode>
 );
